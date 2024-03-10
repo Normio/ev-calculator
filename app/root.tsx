31ed0f46636a66c9e96f9f17cbd2decc825e17ed
Jaakko Normio <jaakko.normio@sleepingwombat.com>
2024-03-10 23:22:20 +0200

    add supabase auth

--- app/root.tsx
+++ app/root.tsx
@@ -1,10 +1,44 @@
+import { LoaderFunctionArgs, json } from "@remix-run/node";
 import {
   Links,
   Meta,
   Outlet,
   Scripts,
   ScrollRestoration,
+  useLoaderData,
 } from "@remix-run/react";
+import { useSupabase } from "utils/supabase";
+import { createServerClient } from "utils/supabase.server";
+
+export const loader = async ({ request }: LoaderFunctionArgs) => {
+  // environment variables may be stored somewhere other than
+  // `process.env` in runtimes other than node
+  // we need to pipe these Supabase environment variables to the browser
+  const env = {
+    SUPABASE_URL: process.env.SUPABASE_URL!,
+    SUPABASE_ANON_KEY: process.env.SUPABASE_ANON_KEY!,
+  };
+
+  // We can retrieve the session on the server and hand it to the client.
+  // This is used to make sure the session is available immediately upon rendering
+  const { supabase, headers } = createServerClient(request);
+
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
+
+  // in order for the set-cookie header to be set,
+  // headers must be returned as part of the loader response
+  return json(
+    {
+      env,
+      session,
+    },
+    {
+      headers: headers,
+    }
+  );
+};
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
@@ -25,5 +59,9 @@ export function Layout({ children }: { children: React.ReactNode }) {
 }
 
 export default function App() {
-  return <Outlet />;
+  const { env, session } = useLoaderData<typeof loader>();
+
+  const { supabase } = useSupabase({ env, session });
+
+  return <Outlet context={{ supabase, session }} />;
 }
