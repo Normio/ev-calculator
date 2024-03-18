584863eeeb2f995d3aa5b7e9fb2b3548210eb0cd
Jaakko Normio <jaakko.normio@sleepingwombat.com>
2024-03-18 20:25:05 +0200

    add redirectTo support

--- app/utils/session.server.ts
+++ app/utils/session.server.ts
@@ -3,17 +3,30 @@ import { createServerClient } from './supabase.server'
 
 export async function getSession(request: Request) {
   const { supabase } = createServerClient(request)
-  const { data } = await supabase.auth.getSession()
+  const { data: {session} } = await supabase.auth.getSession()
 
-  return data.session
+  return session
 }
 
-export async function requireSession(request: Request) {
+export async function requireSession(request: Request, redirectTo: string = new URL(request.url).pathname) {
   const session = await getSession(request)
 
   if (!session) {
-    throw redirect('/')
+    const searchParams = new URLSearchParams([['redirectTo', redirectTo]])
+    throw redirect(`/login?${searchParams}`)
   }
 
   return session
+}
+
+export async function requireUser(request: Request, redirectTo: string = new URL(request.url).pathname) {
+  const { supabase } = createServerClient(request)
+  const { data: {user} } = await supabase.auth.getUser();
+
+  if (!user) {
+    const searchParams = new URLSearchParams([['redirectTo', redirectTo]])
+    throw redirect(`/login?${searchParams}`)
+  }
+  
+  return user
 }
